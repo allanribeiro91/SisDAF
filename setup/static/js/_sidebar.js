3674aad8b0e630973eac6da5e62afbee4528459f
Allan Kássio de Oliveira S. Ribeiro <allan.ribeiro@cna.org.br>
2023-08-30 08:51:33 -0300

    Ajuste na sidebar.

--- setup/static/js/_sidebar.js
+++ setup/static/js/_sidebar.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     const toggleButton = document.getElementById("toggleButton");
     const sidebar = document.querySelector(".sidebar");
+    const workspace = document.querySelector(".workspace");
     const icon = toggleButton.querySelector("i");
 
     toggleButton.addEventListener("click", function(event) {
         event.preventDefault();
-        if (sidebar.style.width === "30px") {
-            sidebar.style.width = "12%";
+        if (sidebar.style.flexBasis === "30px") {
+            sidebar.style.flexBasis = "12%";
             sidebar.classList.remove("collapsed");
             icon.className = "fas fa-arrow-left";
         } else {
-            sidebar.style.width = "30px";
+            sidebar.style.flexBasis = "30px";
             sidebar.classList.add("collapsed");
             icon.className = "fas fa-arrow-right";
         }
