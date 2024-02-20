50f725e5f102f9dd1a9c050bb1101b0260977725
Allan Ribeiro <akosr91@gmail.com>
2024-02-20 20:41:12 -0300

    mobile avanços - módulos

--- apps/mobile/urls.py
+++ apps/mobile/urls.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from django.urls import path
-from apps.mobile.views import (mobile_login, mobile_home)
+from apps.mobile.views import (mobile_login, mobile_home, mobile_modulos,
+                               mobile_numeros, mobile_opcoes,
+                               mobile_modulo_produtosdaf)
 
 urlpatterns = [
 
     #MOBILE
     path('mobile/', mobile_login, name='mobile'),
-    path('mobile/home', mobile_home, name='mobile_home'),
-    
+    path('mobile/home/', mobile_home, name='mobile_home'),
+    path('mobile/modulos/', mobile_modulos, name='mobile_modulos'),
+    path('mobile/numeros/', mobile_numeros, name='mobile_numeros'),
+    path('mobile/opcoes/', mobile_opcoes, name='mobile_opcoes'),
+
+    #MÓDULO PRODUTOS DAF
+    path('mobile/modulos/produtosdaf/', mobile_modulo_produtosdaf, name='mobile_modulo_produtosdaf'),
 
 ]
--- apps/mobile/views.py
+++ apps/mobile/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
 def mobile_login(request):
-    return render(request, 'mobile/main/login.html')
+    return render(request, 'mobile/abas/login.html')
 
 def mobile_home(request):
-    return render(request, 'mobile/main/aba_home.html')
+    return render(request, 'mobile/abas/aba_home.html')
+
+def mobile_modulos(request):
+    return render(request, 'mobile/abas/aba_modulos.html')
+
+def mobile_numeros(request):
+    return render(request, 'mobile/abas/aba_numeros.html')
+
+def mobile_opcoes(request):
+    return render(request, 'mobile/abas/aba_opcoes.html')
+
+def mobile_modulo_produtosdaf(request):
+    nome_modulo = "Produtos Farmacêuticos DAF"
+
+    conteudo = {
+        'nome_modulo': nome_modulo,
+    }
+
+    return render(request, 'mobile/modulo_produtos_daf/produtos_daf.html', conteudo)
