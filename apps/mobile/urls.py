c7309435d3f0b8ed3c4a3e95f36719c3fd8c3308
Allan Ribeiro <akosr91@gmail.com>
2024-02-20 11:54:37 -0300

    mobile aba home

--- apps/mobile/urls.py
+++ apps/mobile/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from apps.mobile.views import (mobile_login,)
+from apps.mobile.views import (mobile_login, mobile_home)
 
 urlpatterns = [
 
     #MOBILE
     path('mobile/', mobile_login, name='mobile'),
+    path('mobile/home', mobile_home, name='mobile_home'),
     
 
 ]
--- apps/mobile/views.py
+++ apps/mobile/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 def mobile_login(request):
-    
     return render(request, 'mobile/main/login.html')
+
+def mobile_home(request):
+    return render(request, 'mobile/main/aba_home.html')
